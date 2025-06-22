86028b326975582ffb8a4f298db1507b7b360539
Mirko Lindroth <mirko.lindroth@gmail.com>
2025-06-22 10:11:46 +0300

    fix: Fix formatting example
    Fix formatting example to use vformat instead of format and providing
    new arguments for the new format specifiers.

--- examples/ex_formatter1.c
+++ examples/ex_formatter1.c
@@ -12,18 +12,20 @@ void my_print(const char *message,...)
 	va_list args;
 
 	// Format string to use
-	const char fmt_str[] = "[app] %(datetime) %(file)[%(func)](%(line)): \n"
-		"[\033[32m INFO \033[0m] %(message)\n";
+	const char fmt_str[] = "[%(app)] %(datetime) %(file)[%(func)](%(line)): \n"
+		"[\033[32m %(msg_type) \033[0m] %(message)\n";
 
 	// Calculate the needed size
 	va_start(args,message);
-	size_t bufsz = format(NULL,fmt_str,__FILE__,__func__,__LINE__,message,args);
+	size_t bufsz = vformat(NULL,fmt_str,__FILE__,__func__,__LINE__,
+			"clogger","INFO","%Y-%m-%d %Z %H:%M:%S",message,args);
 	va_end(args);
 
 	// Store the processed format string
 	char *msg = malloc((bufsz+1)*sizeof(char));
 	va_start(args,message);
-	vformat(msg,fmt_str,__FILE__,__func__,__LINE__,message,args);
+	vformat(msg,fmt_str,__FILE__,__func__,__LINE__,"clogger",
+			"INFO","%Y-%m-%d %Z %H:%M:%S",message,args);
 	va_end(args);
 
 	// Print the message to stdout
@@ -36,7 +38,6 @@ int main()
 {
 	my_print("Hello World!");
 	int x = 1;
-	printf("x is %d\n",x);
-	my_print("Value of x is %d\n",x);
+	my_print("Value of x is %d",x);
 	return EXIT_SUCCESS;
 }
